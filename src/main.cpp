acb7521b97e4f1b1edaca97507b791ebbfe7fb00
Davide Costantini <d.costantini6@campus.unimib.it>
2024-05-21 19:14:36 +0200

    Implemented Gradient Method

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "types.h"
 #include "GaussSeidel.h"
 #include "Jacobi.h"
+#include "GradientMethod.h"
 #include "libs/unsupported/Eigen/SparseExtra"
 #include "Utils.h"
 
@@ -17,14 +18,21 @@ int main()
     // Initializing the initial guess
     Eigen::VectorXf x0;
     x0.resize(matrix.cols());
-    x0.setOnes();
+    x0.setZero();
 
     // Initializing the b vector
     Eigen::VectorXf b;
     b.resize(matrix.cols());
     b.setOnes();
 
-    //GaussSeidel::Result r = GaussSeidel::solve(matrix, b, x0, 0.001f);
+    GaussSeidel::Result rgs = GaussSeidel::solve(matrix, b, x0, 0.001f);
+    std::cout << "[GaussSeidel::solve] x: " << rgs.x.transpose() << std::endl;
+    std::cout << "[GaussSeidel::solve] residual: " << rgs.residual.transpose() << std::endl << std::endl;
+
+    GradientMethod::Result rgm = GradientMethod::solve(matrix, b, x0, 0.001f);
+    std::cout << "[GradientMethod::solve] x: " << rgm.x.transpose() << std::endl;
+    std::cout << "[GradientMethod::solve] residual: " << rgm.residual.transpose() << std::endl << std::endl;
+
     Jacobi::Result r = Jacobi::solve(matrix, b, x0, 0.001f);
 
     return 0;
