61ef9906c77a8eba872525868afbcdb9a32abade
Davide Costantini <davidecostantini30@gmail.com>
2024-05-30 10:21:25 +0200

    Computing relative error using an all-ones initial guess

--- src/main.cpp
+++ src/main.cpp
@@ -15,26 +15,35 @@ int main()
     {
         throw std::invalid_argument("Matrix not valid");
     }
+
     // Initializing the initial guess
     Eigen::VectorXf x0;
     x0.resize(matrix.cols());
     x0.setZero();
 
     // Initializing the b vector
+    Eigen::VectorXf x;
+    x.resize(matrix.cols());
+    x.setOnes();
     Eigen::VectorXf b;
-    b.resize(matrix.cols());
-    b.setOnes();
+    b = matrix * x;
 
     GaussSeidel::Result rgs = GaussSeidel::solve(matrix, b, x0, 0.001f);
     std::cout << "[GaussSeidel::solve] x: " << rgs.x.transpose() << std::endl;
-    std::cout << "[GaussSeidel::solve] residual: " << rgs.residual.transpose() << std::endl << std::endl;
+    std::cout << "[GaussSeidel::solve] residual: " << rgs.residual.transpose() << std::endl;
+    float gserror = Utils::euclideanNorm(rgs.x, x) / Utils::euclideanNorm(x);
+    std::cout << "[GaussSeidel::solve] Relative error: " << gserror << std::endl << std::endl;
 
     GradientMethod::Result rgm = GradientMethod::solve(matrix, b, x0, 0.001f);
     std::cout << "[GradientMethod::solve] x: " << rgm.x.transpose() << std::endl;
-    std::cout << "[GradientMethod::solve] residual: " << rgm.residual.transpose() << std::endl << std::endl;
+    std::cout << "[GradientMethod::solve] residual: " << rgm.residual.transpose() << std::endl;
+    float gmerror = Utils::euclideanNorm(rgm.x, x) / Utils::euclideanNorm(x);
+    std::cout << "[GaussSeidel::solve] Relative error: " << gmerror << std::endl << std::endl;
 
     Jacobi::Result rjm = Jacobi::solve(matrix, b, x0, 0.001f);
     std::cout << "[JacobiMethod::solve] x: " << rjm.x.transpose() << std::endl;
-    std::cout << "[JacobitMethod::solve] residual: " << rjm.residual.transpose() << std::endl << std::endl;
+    std::cout << "[JacobiMethod::solve] residual: " << rjm.residual.transpose() << std::endl;
+    float jmerror = Utils::euclideanNorm(rjm.x, x) / Utils::euclideanNorm(x);
+    std::cout << "[JacobiMethod::solve] Relative error: " << jmerror << std::endl << std::endl;
     return 0;
 }
